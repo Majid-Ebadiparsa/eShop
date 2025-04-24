c6463410c636c04911c7b5fa481ceda3e79bc0a8
MajidEbadiParsa <MajidEbadiparsaJob@gmail.com>
2025-04-25 00:48:50 +0200

    Adding OrderService.Tests project

---
+++ OrderService/OrderService.Tests/OrderTests.cs
@@ -0,0 +1,17 @@
+﻿using OrderService.Domain.AggregatesModel;
+
+namespace OrderService.Tests
+{
+	public class OrderTests
+	{
+		[Fact]
+		public void AddItem_Should_Add_Product_To_Order()
+		{
+			var order = new Order(Guid.NewGuid(), new Address("Elm St", "Frankfurt", "12345"));
+			order.AddItem(Guid.NewGuid(), 2, 100);
+
+			Assert.Single(order.Items);
+			Assert.Equal(200, order.TotalAmount);
+		}
+	}
+}

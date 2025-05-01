6c14ac543dbe33f4ae6c357352ad4888aec4ed8b
MajidEbadiParsa <MajidEbadiparsaJob@gmail.com>
2025-05-02 00:39:26 +0200

    Update inventory API with new controllers and queries
    - Changed launch URL in launchSettings.json to "swagger".
    - Added BaseApiController for shared functionality.
    - Created InventoryController to handle inventory requests.
    - Introduced GetInventoryByProductIdQuery for querying inventory.
    - Implemented GetInventoryByProductIdQueryHandler to process the query.

---
+++ InventoryService/InventoryService.Application/Queries/GetInventoryByProductIdQuery.cs
@@ -0,0 +1,7 @@
+﻿using InventoryService.Domain.AggregatesModel;
+using MediatR;
+
+namespace InventoryService.Application.Queries
+{
+	public record GetInventoryByProductIdQuery(Guid ProductId) : IRequest<InventoryItem?>;
+}
---
+++ InventoryService/InventoryService.Application/Queries/GetInventoryByProductIdQueryHandler.cs
@@ -0,0 +1,21 @@
+﻿using InventoryService.Application.Interfaces;
+using InventoryService.Domain.AggregatesModel;
+using MediatR;
+
+namespace InventoryService.Application.Queries
+{
+	public class GetInventoryByProductIdQueryHandler : IRequestHandler<GetInventoryByProductIdQuery, InventoryItem?>
+	{
+		private readonly IInventoryRepository _repository;
+
+		public GetInventoryByProductIdQueryHandler(IInventoryRepository repository)
+		{
+			_repository = repository;
+		}
+
+		public async Task<InventoryItem?> Handle(GetInventoryByProductIdQuery request, CancellationToken cancellationToken)
+		{
+			return await _repository.GetByProductIdAsync(request.ProductId, cancellationToken);
+		}
+	}
+}

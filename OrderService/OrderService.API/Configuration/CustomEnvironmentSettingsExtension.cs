f16233115fa8dd10eee72616a5552d0a3cb9ed19
MajidEbadiParsa <MajidEbadiparsaJob@gmail.com>
2025-06-22 14:22:51 +0200

    Update environment configurations and Docker setups
    - Added environment variables for database and RabbitMQ settings in deploy files, sourced from GitHub secrets.
    - Removed dynamic loading of environment-specific appsettings in CustomEnvironmentSettingsExtension.cs.
    - Updated project files to eliminate references to appsettings.Development.json and appsettings.Docker.json.
    - Enhanced launchSettings.json with additional environment variables for both services.
    - Modified docker-compose files to include environment variables for dynamic configuration.
    - Introduced docker-compose.override.yml for local development with hardcoded connection strings.

--- OrderService/OrderService.API/Configuration/CustomEnvironmentSettingsExtension.cs
+++ OrderService/OrderService.API/Configuration/CustomEnvironmentSettingsExtension.cs
@@ -6,14 +6,11 @@ namespace OrderService.API.Configuration
 	{
 		public static IServiceCollection AddCustomEnvironmentSettings(this IServiceCollection services, IConfiguration configuration)
 		{
-			var environment = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT") ?? "Development";
-
 			if (configuration is ConfigurationManager configManager)
 			{
 				configManager
 					.SetBasePath(Directory.GetCurrentDirectory())
 					.AddJsonFile("appsettings.json", optional: false)
-					.AddJsonFile($"appsettings.{environment}.json", optional: true)
 					.AddEnvironmentVariables();
 			}
 

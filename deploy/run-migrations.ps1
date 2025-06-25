9ddc977bb3e1879395a69764720d3af31e2b2493
MajidEbadiParsa <MajidEbadiparsaJob@gmail.com>
2025-06-25 10:59:57 +0200

    Refactor SQL migration script path handling
    Updated the path construction for SQL migration scripts to use a dynamic variable `$containerScriptPath`, replacing backslashes with forward slashes. This change improves flexibility and compatibility across different environments.

--- deploy/run-migrations.ps1
+++ deploy/run-migrations.ps1
@@ -5,12 +5,14 @@ $SqlServerContainerIp = docker inspect -f '{{range .NetworkSettings.Networks}}{{
 Get-ChildItem -Path "deploy/sql/OrderDb" -Filter *.sql | Sort-Object Name | ForEach-Object {
     $scriptName = $_.Name
     Write-Host "Applying $scriptName for OrderDb..."
-    docker exec sql-migrator /opt/mssql-tools/bin/sqlcmd -S $SqlServerContainerIp -U sa -P $env:SQLSERVER_SA_PASSWORD -d master -i "/sql/OrderDb/$scriptName"
+    $containerScriptPath = "/sql/OrderDb/$scriptName" -replace '\\','/'
+    docker exec sql-migrator /opt/mssql-tools/bin/sqlcmd -S $SqlServerContainerIp -U sa -P $env:SQLSERVER_SA_PASSWORD -d master -i "$containerScriptPath"
 }
 
 # Apply migrations for InventoryDb
 Get-ChildItem -Path "deploy/sql/InventoryDb" -Filter *.sql | Sort-Object Name | ForEach-Object {
     $scriptName = $_.Name
     Write-Host "Applying $scriptName for InventoryDb..."
-    docker exec sql-migrator /opt/mssql-tools/bin/sqlcmd -S $SqlServerContainerIp -U sa -P $env:SQLSERVER_SA_PASSWORD -d master -i "/sql/InventoryDb/$scriptName"
+    $containerScriptPath = "/sql/InventoryDb/$scriptName" -replace '\\','/'
+    docker exec sql-migrator /opt/mssql-tools/bin/sqlcmd -S $SqlServerContainerIp -U sa -P $env:SQLSERVER_SA_PASSWORD -d master -i "$containerScriptPath"
 }

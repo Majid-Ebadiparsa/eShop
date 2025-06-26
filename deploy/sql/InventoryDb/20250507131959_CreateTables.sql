9409ab822f229fb598ac75761c3f98bb4213c0b2
MajidEbadiParsa <MajidEbadiparsaJob@gmail.com>
2025-06-26 23:24:30 +0200

    Add migration script for Orders and OrderItem tables
    This commit introduces a new migration script that creates the `Orders` and `OrderItem` tables. It checks for the existence of the `__EFMigrationsHistory` table and creates it if necessary. The `Orders` table includes a primary key and relevant order details, while the `OrderItem` table features a foreign key reference to `Orders`. An index is also created on the `OrderItem` table for the `OrderId` field, and the migration history is updated accordingly.

--- deploy/sql/InventoryDb/20250507131959_CreateTables.sql
+++ deploy/sql/InventoryDb/20250507131959_CreateTables.sql
@@ -1,1 +1,49 @@
-﻿
+﻿IF OBJECT_ID(N'[__EFMigrationsHistory]') IS NULL
+BEGIN
+    CREATE TABLE [__EFMigrationsHistory] (
+        [MigrationId] nvarchar(150) NOT NULL,
+        [ProductVersion] nvarchar(32) NOT NULL,
+        CONSTRAINT [PK___EFMigrationsHistory] PRIMARY KEY ([MigrationId])
+    );
+END;
+GO
+
+BEGIN TRANSACTION;
+IF NOT EXISTS (
+    SELECT * FROM [__EFMigrationsHistory]
+    WHERE [MigrationId] = N'20250507131959_Init'
+)
+BEGIN
+    CREATE TABLE [InventoryItems] (
+        [Id] uniqueidentifier NOT NULL,
+        [ProductId] uniqueidentifier NOT NULL,
+        [Quantity] int NOT NULL,
+        CONSTRAINT [PK_InventoryItems] PRIMARY KEY ([Id])
+    );
+END;
+
+IF NOT EXISTS (
+    SELECT * FROM [__EFMigrationsHistory]
+    WHERE [MigrationId] = N'20250507131959_Init'
+)
+BEGIN
+    IF EXISTS (SELECT * FROM [sys].[identity_columns] WHERE [name] IN (N'Id', N'ProductId', N'Quantity') AND [object_id] = OBJECT_ID(N'[InventoryItems]'))
+        SET IDENTITY_INSERT [InventoryItems] ON;
+    EXEC(N'INSERT INTO [InventoryItems] ([Id], [ProductId], [Quantity])
+    VALUES (''3fa85f64-5717-4562-b3fc-2c963f66afa6'', ''11111111-1111-1111-1111-111111111111'', 100)');
+    IF EXISTS (SELECT * FROM [sys].[identity_columns] WHERE [name] IN (N'Id', N'ProductId', N'Quantity') AND [object_id] = OBJECT_ID(N'[InventoryItems]'))
+        SET IDENTITY_INSERT [InventoryItems] OFF;
+END;
+
+IF NOT EXISTS (
+    SELECT * FROM [__EFMigrationsHistory]
+    WHERE [MigrationId] = N'20250507131959_Init'
+)
+BEGIN
+    INSERT INTO [__EFMigrationsHistory] ([MigrationId], [ProductVersion])
+    VALUES (N'20250507131959_Init', N'9.0.4');
+END;
+
+COMMIT;
+GO
+
---
+++ deploy/sql/OrderDb/20250507131959_CreateTables.sql
@@ -0,0 +1,63 @@
+﻿IF OBJECT_ID(N'[__EFMigrationsHistory]') IS NULL
+BEGIN
+    CREATE TABLE [__EFMigrationsHistory] (
+        [MigrationId] nvarchar(150) NOT NULL,
+        [ProductVersion] nvarchar(32) NOT NULL,
+        CONSTRAINT [PK___EFMigrationsHistory] PRIMARY KEY ([MigrationId])
+    );
+END;
+GO
+
+BEGIN TRANSACTION;
+IF NOT EXISTS (
+    SELECT * FROM [__EFMigrationsHistory]
+    WHERE [MigrationId] = N'20250501195459_Init'
+)
+BEGIN
+    CREATE TABLE [Orders] (
+        [Id] uniqueidentifier NOT NULL,
+        [CustomerId] uniqueidentifier NOT NULL,
+        [ShippingAddress_Street] nvarchar(max) NOT NULL,
+        [ShippingAddress_City] nvarchar(max) NOT NULL,
+        [ShippingAddress_PostalCode] nvarchar(max) NOT NULL,
+        [OrderDate] datetime2 NOT NULL,
+        CONSTRAINT [PK_Orders] PRIMARY KEY ([Id])
+    );
+END;
+
+IF NOT EXISTS (
+    SELECT * FROM [__EFMigrationsHistory]
+    WHERE [MigrationId] = N'20250501195459_Init'
+)
+BEGIN
+    CREATE TABLE [OrderItem] (
+        [Id] uniqueidentifier NOT NULL,
+        [ProductId] uniqueidentifier NOT NULL,
+        [Quantity] int NOT NULL,
+        [UnitPrice] decimal(18,2) NOT NULL,
+        [OrderId] uniqueidentifier NULL,
+        CONSTRAINT [PK_OrderItem] PRIMARY KEY ([Id]),
+        CONSTRAINT [FK_OrderItem_Orders_OrderId] FOREIGN KEY ([OrderId]) REFERENCES [Orders] ([Id])
+    );
+END;
+
+IF NOT EXISTS (
+    SELECT * FROM [__EFMigrationsHistory]
+    WHERE [MigrationId] = N'20250501195459_Init'
+)
+BEGIN
+    CREATE INDEX [IX_OrderItem_OrderId] ON [OrderItem] ([OrderId]);
+END;
+
+IF NOT EXISTS (
+    SELECT * FROM [__EFMigrationsHistory]
+    WHERE [MigrationId] = N'20250501195459_Init'
+)
+BEGIN
+    INSERT INTO [__EFMigrationsHistory] ([MigrationId], [ProductVersion])
+    VALUES (N'20250501195459_Init', N'9.0.4');
+END;
+
+COMMIT;
+GO
+

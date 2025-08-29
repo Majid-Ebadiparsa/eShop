ac74285587f866121d11e20983708583627bca3f
MajidEbadiParsa <MajidEbadiparsaJob@gmail.com>
2025-08-29 12:08:04 +0200

    Implementing Domain tests

---
+++ InvoiceProcessingService/tests/InvoiceService.Domain.Tests/InvoiceLineTests.cs
@@ -0,0 +1,57 @@
+﻿using FluentAssertions;
+using InvoiceService.Domain.Entities;
+
+namespace InvoiceService.Domain.Tests;
+
+public class InvoiceLineTests
+{
+	[Fact]
+	public void Ctor_Should_Create_InvoiceLine_When_Inputs_Are_Valid()
+	{
+		// Arrange
+		var desc = "Item A";
+		var price = 12.5;
+		var qty = 3;
+
+		// Act
+		var line = new InvoiceLine(desc, price, qty);
+
+		// Assert
+		line.Id.Should().NotBe(Guid.Empty);
+		line.Description.Should().Be(desc);
+		line.Price.Should().Be(price);
+		line.Quantity.Should().Be(qty);
+	}
+
+	[Theory]
+	[InlineData(null)]
+	[InlineData("")]
+	[InlineData("   ")]
+	public void Ctor_Should_Throw_When_Description_Is_NullOrWhiteSpace(string? bad)
+	{
+		Action act = () => new InvoiceLine(bad!, 10.0, 1);
+		act.Should().Throw<ArgumentException>()
+			 .WithParameterName("description");
+	}
+
+	[Theory]
+	[InlineData(0)]
+	[InlineData(-0.0001)]
+	[InlineData(-10)]
+	public void Ctor_Should_Throw_When_Price_Is_Not_Positive(double bad)
+	{
+		Action act = () => new InvoiceLine("desc", bad, 1);
+		act.Should().Throw<ArgumentOutOfRangeException>()
+			 .WithParameterName("price");
+	}
+
+	[Theory]
+	[InlineData(0)]
+	[InlineData(-1)]
+	public void Ctor_Should_Throw_When_Quantity_Is_Not_Positive(int bad)
+	{
+		Action act = () => new InvoiceLine("desc", 1.0, bad);
+		act.Should().Throw<ArgumentOutOfRangeException>()
+			 .WithParameterName("quantity");
+	}
+}
--- InvoiceProcessingService/tests/InvoiceService.Domain.Tests/InvoiceTests.cs
+++ InvoiceProcessingService/tests/InvoiceService.Domain.Tests/InvoiceTests.cs
@@ -1,34 +1,84 @@
 ﻿using FluentAssertions;
 using InvoiceService.Domain.Entities;
 
-namespace InvoiceService.Domain.Tests
+namespace InvoiceService.Domain.Tests;
+
+public class InvoiceTests
 {
-	public class InvoiceTests
+	private static InvoiceLine L(string d = "item", double p = 1.0, int q = 1)
+			=> new InvoiceLine(d, p, q);
+
+	[Fact]
+	public void Ctor_Should_Create_Invoice_When_Inputs_Are_Valid()
 	{
-		[Fact]
-		public void Creating_Invoice_Without_Lines_Should_Throw()
-		{
-			var act = () => new Invoice("desc", DateTime.UtcNow.AddDays(2), "ACME", Enumerable.Empty<InvoiceLine>());
-			act.Should().Throw<ArgumentException>().WithMessage("*At least one line*");
-		}
+		// Arrange
+		var description = "Office Order";
+		var supplier = "ACME GmbH";
+		var due = DateTime.UtcNow.AddDays(7);
+		var lines = new List<InvoiceLine> { L("A", 2.5, 2), L("B", 1.2, 5) };
 
-		[Fact]
-		public void Creating_InvoiceLine_With_NonPositive_Values_Should_Throw()
-		{
-			Action p = () => new InvoiceLine("x", 0, 1);
-			Action q = () => new InvoiceLine("x", 1, 0);
-			p.Should().Throw<ArgumentOutOfRangeException>();
-			q.Should().Throw<ArgumentOutOfRangeException>();
-		}
+		// Act
+		var invoice = new Invoice(description, due, supplier, lines);
 
-		[Fact]
-		public void Valid_Invoice_Should_Be_Created()
-		{
-			var line = new InvoiceLine("A", 1.5, 2);
-			var inv = new Invoice("desc", DateTime.UtcNow.AddDays(3), "ACME", new[] { line });
+		// Assert
+		invoice.Id.Should().NotBe(Guid.Empty);
+		invoice.Description.Should().Be(description);
+		invoice.Supplier.Should().Be(supplier);
+		invoice.DueDate.Should().BeCloseTo(due, precision: TimeSpan.FromSeconds(1));
+		invoice.Lines.Should().HaveCount(lines.Count);
 
-			inv.Id.Should().NotBe(Guid.Empty);
-			inv.Lines.Should().HaveCount(1);
-		}
+		// Make sure Lines is read-only
+		var coll = (ICollection<InvoiceLine>)invoice.Lines;
+		coll.IsReadOnly.Should().BeTrue("Lines must be read-only");
+		Action mutate = () => coll.Add(L());
+		mutate.Should().Throw<NotSupportedException>();
+		((object)invoice.Lines).Should().NotBeOfType<List<InvoiceLine>>();
+	}
+
+	[Theory]
+	[InlineData(null)]
+	[InlineData("")]
+	[InlineData("   ")]
+	public void Ctor_Should_Throw_When_Description_Is_NullOrWhiteSpace(string? bad)
+	{
+		Action act = () => new Invoice(bad!, DateTime.UtcNow, "Supplier", new[] { L() });
+		act.Should().Throw<ArgumentException>()
+			 .WithParameterName("description");
+	}
+
+	[Theory]
+	[InlineData(null)]
+	[InlineData("")]
+	[InlineData("   ")]
+	public void Ctor_Should_Throw_When_Supplier_Is_NullOrWhiteSpace(string? bad)
+	{
+		Action act = () => new Invoice("desc", DateTime.UtcNow, bad!, new[] { L() });
+		act.Should().Throw<ArgumentException>()
+			 .WithParameterName("supplier");
+	}
+
+	[Fact]
+	public void Ctor_Should_Throw_When_Lines_Is_Null()
+	{
+		Action act = () => new Invoice("desc", DateTime.UtcNow, "supp", null!);
+		act.Should().Throw<ArgumentException>()
+			 .WithParameterName("lines");
+	}
+
+	[Fact]
+	public void Ctor_Should_Throw_When_Lines_Is_Empty()
+	{
+		Action act = () => new Invoice("desc", DateTime.UtcNow, "supp", Enumerable.Empty<InvoiceLine>());
+		act.Should().Throw<ArgumentException>()
+			 .WithParameterName("lines");
+	}
+
+	[Fact]
+	public void Ctor_Should_Trim_Description_And_Supplier()
+	{
+		var invoice = new Invoice("  desc  ", DateTime.UtcNow, "  supp  ", new[] { L() });
+
+		invoice.Description.Should().Be("desc");
+		invoice.Supplier.Should().Be("supp");
 	}
 }

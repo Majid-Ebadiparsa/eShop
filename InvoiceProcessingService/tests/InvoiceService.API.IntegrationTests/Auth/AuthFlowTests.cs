79c5e2115a4479b962a2e6fcacde24ec7e624108
MajidEbadiParsa <MajidEbadiparsaJob@gmail.com>
2025-08-30 16:48:41 +0200

    InvoiceService.API.IntegrationTests

---
+++ InvoiceProcessingService/tests/InvoiceService.API.IntegrationTests/Auth/AuthFlowTests.cs
@@ -0,0 +1,21 @@
+﻿using FluentAssertions;
+using InvoiceService.API.IntegrationTests.Helpers;
+
+namespace InvoiceService.API.IntegrationTests.Auth
+{
+	public class AuthFlowTests : IClassFixture<TestWebAppFactory>
+	{
+		private readonly TestWebAppFactory _factory;
+		public AuthFlowTests(TestWebAppFactory factory) => _factory = factory;
+
+		[Fact]
+		public async Task Should_Issue_Jwt_Token_For_Demo_User()
+		{
+			using var client = _factory.CreateClient();
+			var (token, expires) = await client.GetDemoTokenAsync();
+
+			token.Should().NotBeNullOrWhiteSpace();
+			expires.Should().BeGreaterThan(0);
+		}
+	}
+}
